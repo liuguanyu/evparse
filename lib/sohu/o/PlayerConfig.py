79a979183b12d179448ffc2101c45a742bdcee48
sceext <sceext@foxmail.com>
2015-05-01 16:54:34 +0800

    sohu, frist stage partly finished

---
+++ lib/sohu/o/PlayerConfig.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+# PlayerConfig.py, part for evparse : EisF Video Parse, evdh Video Parse. 
+# PlayerConfig: sohu, com.sohu.tv.mediaplayer
+
+class PlayerConfig(object):
+    
+    FETCH_VINFO_PATH = 'http://hot.vrs.sohu.com/vrs_flash.action?vid='
+    FETCH_VINFO_PATH_TRANSITION = 'http://hot.vrs.sohu.com/vrs_vms.action?p=flash&old='
+    FETCH_VINFO_PATH_MYTV = 'http://my.tv.sohu.com/play/videonew.do?vid='
+    FETCH_LIVE_PATH = 'http://live.tv.sohu.com/live/player_json.jhtml?encoding=utf-8&lid='
+    
+    FETCH_VINFO_SUB_IP = ['220.181.118.25','220.181.118.181','123.126.48.47','123.126.48.48']
+    
+    STAT_IP = ['qf1.hd.sohu.com.cn','qf2.hd.sohu.com.cn']
+    STAT_IP_QFCLIPS = ['qf3.hd.sohu.com.cn','qf4.hd.sohu.com.cn','qf5.hd.sohu.com.cn']
+    
+    def __init__(self):
+        self.passportUID = ''
+        self.passportMail = ''
+        self.xuid = ''
+        
+        self.apiKey = ''
+        self.userAgent = ''
+        
+        self.hashId = []
+        self.key = []
+        self.fileSize = []
+        
+        self.clientIp = ''
+        self.cdnIp = ''
+        self.cdnId = ''
+        
+        self.filePrimaryReferer = ''
+        
+        self.vid = ''
+        self.norVid = ''	# 标清
+        self.hdVid = ''		# 高清
+        self.superVid = ''	# 超清
+        self.oriVid = ''	# 原画
+        self.h2644kVid = ''	# 4K, h264
+        self.h2654kVid = ''	# 4K, h265
+        # norVid	标清
+        # highVid	高清
+        # superVid	超清
+        # oriVid	原画
+        # h2644kVid	4K, h264
+        # h265norVid	标清, h265
+        # h265highVid	高清, h265
+        # h265superVid	超清, h265
+        # h265oriVid	原画, h265
+        # h2654mVid	(未知)
+        # h2654kVid	4K, h265
+        
+        self.cid = ''
+        self.relativeId = ''
+        self.currentVid = ''
+        
+        self.sid = ''
+        self.nid = ''
+        self.pid = ''
+        
+        self.userId = ''
+        self.myTvUid = ''
+        self.myTvUserId = ''
+        self.uuid = ''
+        
+        self.tvid = ''
+        
+        self.currentPageUrl = ''
+        self.outReferer = ''
+        
+        self.isLive = False
+        
+        # done
+
+# end PlayerConfig.py
+
+
---
+++ lib/sohu/o/exports.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# exports.py, part for evparse : EisF Video Parse, evdh Video Parse. 
+# exports: exports and imports for lib/sohu/o. 
+
+# import
+
+# import out
+from ... import flash
+from ... import base
+
+# import in
+
+# set in
+#remote_mixer.set_import(flash)
+
+# exports
+
+# end exports.py
+
+
