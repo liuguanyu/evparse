27088a72329bf8808d441d53f1a373040df34f82
sceext <sceext@foxmail.com>
2015-05-17 23:49:08 +0800

    fix pptv 206

--- lib/pptv/get_video_info.py
+++ lib/pptv/get_video_info.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # get_video_info.py, part for evparse : EisF Video Parse, evdh Video Parse. 
 # get_video_info: evparse/lib/pptv
-# version 0.1.0.0 test201505172314
+# version 0.1.1.0 test201505172348
 # author sceext <sceext@foxmail.com> 2009EisF2015, 2015.05. 
 # copyright 2015 sceext
 #
@@ -43,7 +43,6 @@ VIDEO_FT_TO_HD = {	# translate pptv video ft to evparse hd number
 
 FIX_USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0'
 FIX_REFERER = 'http://player.pplive.cn/vodcore/1.3.0.18/VodCore.swf'
-FIX_HTTP_OK_CODE = 206
 
 # base functions
 def number(text):
@@ -87,7 +86,6 @@ def get_file_info(info, flist):
         # add file fix info
         item['user_agent'] = FIX_USER_AGENT
         item['referer'] = FIX_REFERER
-        item['http_ok_code'] = FIX_HTTP_OK_CODE
         # get one file done
         finfo.append(item)
     # done
--- lib/pptv/o/vodcore/PlayInfo.py
+++ lib/pptv/o/vodcore/PlayInfo.py
@@ -38,14 +38,10 @@ class PlayInfo(object):
         _loc5 = None
         if param2 == None:
             param2 = self.host
-        # NOTE fix it, param3 == 0 and param4 == 0
-        # FIX start
-        # if param3 == 0 and param4 == 0:
-        #     _loc5 = 'http://' + param2 + '/' + param1 + '/' + self.fileName
-        # else:
-        # FIX, just add /0/0/ to url
-        _loc5 = 'http://' + param2 + '/' + str(param1) + '/' + str(param3) + '/' + str(param4) + '/' + self.fileName
-        # FIX end
+        if param3 == 0 and param4 == 0:
+            _loc5 = 'http://' + param2 + '/' + param1 + '/' + self.fileName
+        else:
+            _loc5 = 'http://' + param2 + '/' + str(param1) + '/' + str(param3) + '/' + str(param4) + '/' + self.fileName
         _loc5 += '?fpp.ver=' + Version.version
         return self.addVariables(_loc5)
     
