9c26660a4b48a1cc4d06152a300924cb44f151b8
sceext <sceext@foxmail.com>
2015-05-17 22:12:10 +0800

    pptv, get file url, test failed, 403 forbidden

--- lib/pptv/get_video_info.py
+++ lib/pptv/get_video_info.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # get_video_info.py, part for evparse : EisF Video Parse, evdh Video Parse. 
 # get_video_info: evparse/lib/pptv
-# version 0.0.2.0 test201505172206
+# version 0.0.2.1 test201505172208
 # author sceext <sceext@foxmail.com> 2009EisF2015, 2015.05. 
 # copyright 2015 sceext
 #
@@ -28,6 +28,8 @@
 # import
 
 import xml.etree.ElementTree as ET
+
+from .o import exports
 
 # global vars
 
